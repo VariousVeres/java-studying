e680095f2ecf234e2e40b3ff6957751f8dbd8ef4
Oleksandr Veresiuk <whereisveres@gmail.com>
2020-10-18 23:22:04 +0300

    Add comments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # java-studying
 
 This is my testing progect consists of 2 packages:
-1)Some Java features examples
-2)Training package
+1)examples - Some Java features examples
+2)training - Training package
--- src/examples/Lambdas3.java
+++ src/examples/Lambdas3.java
@@ -13,6 +13,8 @@ public class Lambdas3 {
 
     int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
+
+    //Метод приймає масив і екземпляр інтерфейсу і рахує суму елементів масиву, які відповідають умові метода
     public static int sumOfPairInArray(int[] arr, Operation o) {
         int result = 0;
         for (int elem : arr) {
@@ -35,7 +37,7 @@ public class Lambdas3 {
 
     @Test
     public void test() {
-        /**А вже тут ми на льоту реальізовуєм цей метод, і можем кожен раз змінювати умову**/
+        /**А вже тут ми на льоту реалізовуєм цей метод, і можем кожен раз змінювати умову**/
         int sum1 = Lambdas3.sumOfPairInArray(arr, (int a) -> a % 2 == 0);
         int sum2 = Lambdas3.sumOfPairInArray(arr, (int a) -> a % 2 == 1);
         int sum3 = Lambdas3.sumOfPairInArray(arr, op);

72dbf27df26867fdebda71ad08e0f095d741655b
Oleksandr Veresiuk <whereisveres@gmail.com>
2020-12-06 15:43:14 +0200

    Method as Funcional interface parameter

--- src/examples/FunctionalInterfaces.java
+++ src/examples/FunctionalInterfaces.java
@@ -23,9 +23,9 @@ public class FunctionalInterfaces {
     @Test
     public void main() {
         //1)Імплементуєм його через лямбду, вказуєм тип і повертаєм його ж
-        Face1<String> face1 = (stringValue)-> stringValue+"  ";
+        Face1<String> face1 = (stringValue) -> stringValue + "  ";
         //2)Так само імплементуєм його через лямбду, типи не потрібно вказувати - приймає інт вертає булін
-        Face2 face2 = (intValue)-> intValue>8;
+        Face2 face2 = (intValue) -> intValue > 8;
 
         /**Є цілий набір вже готових таких інтерфейсів, які можна імпортити і використовувати**/
         //Приймає один тип, повертає boolean
@@ -33,30 +33,44 @@ public class FunctionalInterfaces {
         Predicate pr2 = (t) -> true;
 
         //Приймає один тип, нічого ен повертає
-        Consumer c = (t) -> System.out.println(t);
+        Consumer c1 = (t) -> System.out.println(t);
 
         //Приймає 2 типи, не повертає нічого
         BiConsumer<String, Boolean> bc = (a, b) -> System.out.println(b);
 
         //Приймає тип double, нічого ен повертає
-        DoubleConsumer dc= (d)-> System.out.println();
+        DoubleConsumer dc = (d) -> System.out.println();
 
         //Приймає один тип, повертає інший
         Function<Integer, String> f1 = (t) -> String.valueOf(t);
         Function<String, Boolean> f2 = (t) -> t.startsWith("");
-        Function f3=(t)-> "";
+        Function f3 = (t) -> "";
 
         //Приймає 2 типи, повертає третій
-        BiFunction<Integer, String,Boolean> bi = (x, y) -> (x+Integer.parseInt(y))>3;
+        BiFunction<Integer, String, Boolean> bi = (x, y) -> (x + Integer.parseInt(y)) > 3;
 
         //Нічого не приймає -повертає якийсь тип
         Supplier s1 = () -> 34;
-        Supplier <String>s2 = () -> "Стрінга";
+        Supplier<String> s2 = () -> "Стрінга";
 
         //Нічого не приймає - повертає boolean
         BooleanSupplier bs = () -> true;
+        //3)Можем викликати наш метод, а реалізацію не робити на льоту лямбдою, а заюзати вже існуючий метод з нашого касу
+        method(Support::meth);
     }
 
+    //3)Складніший кейс - маєм метод, який приймає якийсь з інтерфейсів, наприклад Predicate
+    boolean method(Predicate<String> t) {
+        String s = "asd";
+        return t.test(s);
+    }
+
+}
+//3)І маєм клас, який має метод, який підходить для реалізації метода інтерфейса Predicate, він примає якийсь тип, виконує дію і повертає boolean
+class Support {
+    public static boolean meth(String a) {
+        return a.toUpperCase().equals("Значення");
+    }
 }
 
 

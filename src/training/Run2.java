dc85c9846bf393dea3c67307e8033e030de62c76
VariousVeres <Whereisveres@gmail.com>
2021-11-04 16:14:21 +0200

    Changes

--- src/training/Run2.java
+++ src/training/Run2.java
@@ -3,9 +3,6 @@ package training;
 
 import org.junit.Test;
 
-import java.util.function.BiConsumer;
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleConsumer;
 import java.util.function.Predicate;
 
 /**
@@ -16,11 +13,17 @@ public class Run2 {
     @Test
     public void method() {
         System.out.println("New branch");
+        Pituh p = new Pituh();
+
+        p.summOfArrayElements(new int[]{3, 4, 5, 87, 21}, (a) -> (int) a > 5);
+        p.summOfArrayElements(new int[]{3, 4, 5, 87, 21}, (a) -> (int) a < 5);
     }
+
+
 }
 
 class Pituh {
-    public static int summOfArrayElements(int[] arr, Predicate p) {
+    public int summOfArrayElements(int[] arr, Predicate p) {
         int result = 0;
         for (int elem : arr) {
             if (p.test(elem)) {

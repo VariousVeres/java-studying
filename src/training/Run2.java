cfbf3887ffb6963d31875a62618e9cba8a3ebe72
VariousVeres <Whereisveres@gmail.com>
2021-11-04 14:38:20 +0200

    Changes in branch_1

--- src/training/Run2.java
+++ src/training/Run2.java
@@ -15,25 +15,8 @@ import java.util.function.Predicate;
 public class Run2 {
     @Test
     public void method() {
-    Predicate<Integer> p = (a) -> (a % 2) == 0;
-
-    DoubleConsumer dc = (a) -> System.out.println(a + " ");
-        dc.accept(0.25);
-    BiConsumer bc = (a, b) -> System.out.println((int) a + (int) b);
-        bc.accept(23,45);
-    BooleanSupplier bs = () -> {
-        System.out.println(false);
-        return false;
-    };
-        bs.getAsBoolean();
-
-        Pituh.summOfArrayElements(new int[]
-
-    {
-        3, 4, 5, 6, 34
-    },(a)->(Integer)a %2==0);
-}
-
+        System.out.println("New branch");
+    }
 }
 
 class Pituh {

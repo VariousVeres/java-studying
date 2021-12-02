abbca476021e7560a0deff6fd53c089677bdbe81
VariousVeres <Whereisveres@gmail.com>
2021-12-02 15:59:08 +0200

    Alyona

--- src/examples/JDBC.java
+++ src/examples/JDBC.java
@@ -4,15 +4,17 @@ import org.junit.Test;
 
 import java.sql.*;
 
-/**Приклад найпростішого використання JDBC**/
+/**
+ * Приклад найпростішого використання JDBC
+ **/
 
 public class JDBC {
 
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DATABASE_URL = "jdbc:mysql://localhost/test_db";
+    static final String DATABASE_URL = "";
 
-    static final String USER = "root";
-    static final String PASSWORD = "azAZ09@-";
+    static final String USER = "";
+    static final String PASSWORD = "";
 
     @Test
     public void method() throws SQLException {
@@ -20,11 +22,11 @@ public class JDBC {
         Statement statement = null;
 
         System.out.println("Registering JDBC driver...");
-       try {
-        Class.forName(JDBC_DRIVER).getDeclaredConstructor().newInstance();}
-       catch (Exception e)  {
-           System.out.println(e+"");
-       }
+        try {
+            Class.forName(JDBC_DRIVER).getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            System.out.println(e + "");
+        }
 
         System.out.println("Creating database connection...");
         connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
@@ -33,24 +35,24 @@ public class JDBC {
         statement = connection.createStatement();
 
         String sql;
-        sql = "SELECT * FROM test_db.my_guests";
+        sql = "SELECT * FROM `ebdb`.`additional_question` WHERE `question_id` = 657";
 
         ResultSet resultSet = statement.executeQuery(sql);
 
-        System.out.println("Retrieving data from database...");
-        System.out.println("\nmy_guests:");
+
+        System.out.println(resultSet.toString());
 
         while (resultSet.next()) {
             int id = resultSet.getInt("id");
-            String name = resultSet.getString("firstname");
-            String surname = resultSet.getString("lastname");
-            String email = resultSet.getString("email");
+            String label = resultSet.getString("label");
+            String type = resultSet.getString("type");
+            String position = resultSet.getString("position");
 
             System.out.println("\n================\n");
             System.out.println("id: " + id);
-            System.out.println("Name: " + name);
-            System.out.println("Specialty: " + surname);
-            System.out.println("Salary: $" + email);
+            System.out.println("Name: " + label);
+            System.out.println("Specialty: " + type);
+            System.out.println("Salary: $" + position);
 
             System.out.println("Closing connection and releasing resources...");
 

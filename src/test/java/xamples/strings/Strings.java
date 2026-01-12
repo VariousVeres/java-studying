251568bc6d84bdc58afa5ea4ba292812959829ce
oleksandr.veresiuk <oleksandr.veresiuk@eleks.com>
2026-01-12 13:00:56 +0200

    Strings

--- src/test/java/xamples/strings/Strings.java
+++ src/test/java/xamples/strings/Strings.java
@@ -3,13 +3,32 @@ package xamples.strings;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.Random;
 
 public class Strings {
 
     String upperFluffer = "FLUFFER";
 
     String flufferWithSpaces = "Fluf fer ";
+
+
+    //Генерим рендомну стрінгу
+    public static String randomString(int length) {
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        //Перебираємо стільки ітерацій на ту довжину яку на треба
+        for (int i = 0; i < length; i++) {
+            //Рендом буде генерити кожен раз інт який в баунді довжини строки
+            int index = random.nextInt(str.length());
+            //Тягнем зі строки її чар символ і додаємо дл стрінг білдера
+            char symbol = str.charAt(index);
+
+            sb.append(symbol);
+        }
+        return sb.toString();
+    }
 
 
     @Test
@@ -36,6 +55,11 @@ public class Strings {
         System.out.println(Character.getName(arr[5]));
         //Знаходимо по стрінзі який її індекс
         System.out.println(murzik.indexOf("зі"));
+
+
+        String randomString = randomString(23);
+        System.out.println(randomString);
+
 
     }
 }

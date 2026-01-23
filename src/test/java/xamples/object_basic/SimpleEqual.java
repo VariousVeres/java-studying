86e40988c82da1d86d92b85ac71bd70bc23e0c7e
oleksandr.veresiuk <oleksandr.veresiuk@eleks.com>
2026-01-23 14:40:58 +0200

    Simple equal

--- src/test/java/xamples/object_basic/SimpleEqual.java
+++ src/test/java/xamples/object_basic/SimpleEqual.java
@@ -1,10 +1,13 @@
 package xamples.object_basic;
 
 import org.junit.Test;
-import training_package_another.Cat;
 
 import java.util.HashSet;
 import java.util.Set;
+
+class Bear {
+    String type;
+}
 
 public class SimpleEqual {
     @Test
@@ -13,16 +16,19 @@ public class SimpleEqual {
         int i1 = 2;
         int i2 = 2;
         int i3 = 19;
+        System.out.println("int: ");
         System.out.println(i1 == i2);
         System.out.println(i1 == i3);
         System.out.println(Integer.compare(i1, i2));
         System.out.println(Integer.compare(i1, i3) + "\n");
 
+        System.out.println("boolean: ");
         boolean bool1 = false;
         boolean bool2 = false;
         System.out.println(bool1 == bool2);
-        System.out.println(Boolean.compare(bool1, bool2));
+        System.out.println(Boolean.compare(bool1, bool2) + "\n");
 
+        System.out.println("string: ");
         String s1 = "Johny";
         String s2 = "Johny";
         //Для стрінгів хоч це об'єкти так канає бо хоч і різні референси але дані беруться з одного String pool
@@ -33,23 +39,22 @@ public class SimpleEqual {
         System.out.println(s1 == s3);
 
         //однаковий hashCode НЕ означає, що обʼєкти — той самий обʼєкт
-        System.out.println("Хеші однакові в стрінгів " + s1.hashCode() + " " + s2.hashCode() + " " + s3.hashCode());
+        System.out.println("Хеші однакові в стрінгів " + s1.hashCode() + " " + s2.hashCode() + " " + s3.hashCode() + "\n");
 
+        System.out.println("object: ");
         /** == для об'єктів порівнює посилання на об'єкти чи ті ж самі об'єкти**/
-        Set<String> set1 = new HashSet<>();
-        set1.add("Panda");
-        Set<String> set2 = new HashSet<>();
-        set2.add("Panda");
+        Bear b1 = new Bear();
+        b1.type = "Panda";
+        Bear b2 = new Bear();
+        b2.type = "Panda";
         //Буде false бо це різні посилання на різні об'єкти, хоч об'єкти і логічно рівні
-        System.out.println(set1 == set2);
-        System.out.println(set1.hashCode());
-        System.out.println(set2.hashCode());
-        //А через equals true бо логічно вони рівні бо HashSet працює по хешам і перевизначає hashCode()
-        System.out.println(set1.equals(set2));
+        System.out.println(b1 == b2);
+        System.out.println("Хеші різні в логічно однакових об'єктів " + b1.hashCode() + " " + b2.hashCode());
 
         //Буде true бо це посилання на один й той самий об'єкт
-        Set<String> set3 = set1;
-        System.out.println(set1 == set3);
+        Bear b3 = b1;
+        System.out.println(b1 == b3);
+        System.out.println("Буде той самий хеш що в b1 бо це просто інша лінка яка на нього зсилається " + b3.hashCode());
 
 
     }

b41167775ee17cfa98b6c59637a271c2903fe398
oleksandr.veresiuk <oleksandr.veresiuk@eleks.com>
2026-02-07 23:55:54 +0200

    Generics practice

---
+++ src/test/java/xamples/generics/neww/GenericMethod.java
@@ -0,0 +1,47 @@
+package xamples.generics.neww;
+
+import org.testng.annotations.Test;
+
+class Animal {
+
+}
+
+class Dog extends Animal {
+}
+
+class Cat extends Animal {
+}
+
+class Car {
+}
+
+public class GenericMethod {
+    //Метод який оперує типом <Т> без bounds та повертає саме той тип T який ми передаєм в нього
+
+    <T> T identity(T value) {
+
+        return value;
+    }
+
+    Object identityObj(Object o) {
+        return o;
+    }
+
+    @Test
+    public void test() {
+        Dog dog = identity(new Dog());
+        Cat cat = identity(new Cat());
+        String s = identity("hello");
+        Car car = identity(new Car());
+
+        Dog dog1 = (Dog) identityObj(new Dog());
+//  ❌ Це НЕ має працювати:
+//        int x = identity("hello");
+
+    }
+}
+
+
+
+
+

444529503ad4f3a69a28ddf606ffee9f05b03016
oleksandr.veresiuk <oleksandr.veresiuk@eleks.com>
2026-02-09 14:19:56 +0200

    GenericBoundingClass

---
+++ src/test/java/xamples/generics/neww/GenericBoundingClass.java
@@ -0,0 +1,25 @@
+package xamples.generics.neww;
+
+import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
+import java.time.temporal.Temporal;
+
+class Game<T extends Temporal, E extends RuntimeException>  {
+    T t;
+    E e;
+
+    Game(T type, E exception)  {
+        this.t=type;
+        this.e=exception;
+    }
+}
+
+public class GenericBoundingClass{
+
+    @Test
+    public void test()  {
+        Game<LocalDateTime, NullPointerException> game = new Game<>(LocalDateTime.now(), new NullPointerException());
+        throw game.e;
+    }
+}
